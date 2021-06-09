ddae3449a51a24ce34c1ee421942b00aa2ab3ac3
novena_shao <homawoojoseph@gmail.com>
2021-06-09 21:17:54 +0000

    27 formulaire déb
    methode naive

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -7,6 +7,7 @@ use App\Repository\PinRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PinsController extends AbstractController
 {
@@ -15,10 +16,30 @@ class PinsController extends AbstractController
     public function index(PinRepository $repo): Response
     {
       
-        
+       
        
        #ne fonctione que au niveau des controler
 
         return $this->render('pins/index.html.twig',['pins'=>$repo->findAll()]);
+    }
+    public function create(Request $request,EntityManagerInterface $em): Response
+    {
+        if($request->isMethod('POST')){
+           $data= $request->request->all();
+           $pin = new Pin();
+           $pin->setTitle($data['title']);
+           $pin->setDescription($data['description']);
+
+
+           $em->persist($pin);
+           
+           $em->flush();
+           return $this->redirect('/');
+        }
+        
+       
+       #ne fonctione que au niveau des controler
+
+        return $this->render('pins/create.html.twig');
     }
 }

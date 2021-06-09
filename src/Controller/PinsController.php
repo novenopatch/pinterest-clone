d207efbac79060bd57ae0158008eec7ede0316cc
novena_shao <homawoojoseph@gmail.com>
2021-06-09 13:31:36 +0000

    Update PinsController.php
    25 go

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -14,18 +14,8 @@ class PinsController extends AbstractController
     public function index(EntityManagerInterface $em): Response
     {
       
-        #return new Response('<h1>hello word</>');
-        $pin = new Pin();
-        $pin1 = new Pin();
-        $pin->setTitle('Title 1');
-        $pin1->setDescription('description 1');
-        $pin1->setTitle('Title 2');
-        $pin->setDescription('description 2');
-       
-        $em->persist($pin);
-        $em->persist($pin1);
-        $em->flush();
-        dump($pin);
+        
+        
         return $this->render('pins/index.html.twig');
     }
 }

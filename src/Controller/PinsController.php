cfc38f37bed4106230a107216b2ef3aaacc51ca0
novena_shao <homawoojoseph@gmail.com>
2021-06-10 06:44:42 +0000

    end 29
    29 end super formulaire

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -7,6 +7,9 @@ use App\Repository\PinRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class PinsController extends AbstractController
@@ -24,27 +27,25 @@ class PinsController extends AbstractController
     }
     public function create(Request $request,EntityManagerInterface $em): Response
     {
-        if($request->isMethod('POST')){
-           $data= $request->request->all();
-           if($this->isCsrfTokenValid('pins_create',$data['_token'])){
-            $pin = new Pin();
+        $form = $this->createFormBuilder()
+            ->add('title', TextType::class)
+            ->add('description',TextareaType::class)
+            ->add('submit',SubmitType::class,['label'=>'Create Pin'])
+            ->getForm()
+        ;
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            $pin = new Pin;
             $pin->setTitle($data['title']);
             $pin->setDescription($data['description']);
- 
- 
             $em->persist($pin);
-            
             $em->flush();
-           }
-           
-           #return $this->redirect($this->generateUrl('app_home'));
-           return $this->redirect('https://www.google.com');
-           return $this->redirectToRoute('app_home');
-        }
-        
-       
-       #ne fonctione que au niveau des controler
 
-        return $this->render('pins/create.html.twig');
+            return $this->redirectToRoute('app_home');
+
+        }
+
+        return $this->render('pins/create.html.twig',['form'=> $form->createView()]);
     }
 }
